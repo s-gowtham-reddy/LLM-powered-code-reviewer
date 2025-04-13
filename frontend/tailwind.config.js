f756502c78934d052729f4f7a152f6d172bf996f
s-gowtham-reddy <sgowthamreddy01@gmail.com>
2025-04-14 01:35:40 +0530

    Restore deleted frontend files

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  darkMode: 'class', // <-- This ensures the dark mode is triggered by a class
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
